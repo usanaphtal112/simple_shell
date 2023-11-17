8724826ae69ff1ccb3bb66ce17d255c822f67bb3
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-11-17 15:28:19 -0800

    Generate and display an error message implementations

--- get_error_shell.c
+++ get_error_shell.c
@@ -1,42 +1,34 @@
 #include "shell.h"
 
 /**
- * get_error - Generate and display an error message
- * based on the evaluation code.
- * @simpdata: Pointer to the simple_shell_d structure.
- * @eval: Evaluation code indicating the type of error.
+ * @brief Generate and display an error message based on the evaluation code.
  *
- * This function generates an error message based
- *  on the provided evaluation code and displays it on the
- * standard error output. The evaluation code corresponds
- * to specific error scenarios, such as environment-related errors
+ * This function generates an error message based on the provided evaluation code
+ * and displays it on the standard error output. The evaluation code corresponds
+ * to specific error scenarios, such as environment-related errors,
  * permission denied, command not found, and exit shell errors.
- * The resulting error message is dynamically
- * allocated and should be freed by the caller.
+ * The resulting error message is dynamically allocated and should be freed by the caller.
  *
- * @return: The provided evaluation code.
+ * @param simpdata Pointer to the simple_shell_d structure.
+ * @param eval Evaluation code indicating the type of error.
+ * @return The provided evaluation code.
  */
 int get_error(simple_shell_d *simpdata, int eval)
 {
-    char *error;
+    char *error = NULL;
 
-    switch (eval)
-    {
-    case -1:
+    if (eval == -1)
         error = error_env(simpdata);
-        break;
-    case 126:
+    else if (eval == 126)
         error = error_path_126(simpdata);
-        break;
-    case 127:
+    else if (eval == 127)
         error = error_not_found(simpdata);
-        break;
-    case 2:
+    else if (eval == 2)
+    {
         if (_strcmp("exit", simpdata->args[0]) == 0)
             error = error_exit_shell(simpdata);
         else if (_strcmp("cd", simpdata->args[0]) == 0)
             error = error_get_cd(simpdata);
-        break;
     }
 
     if (error)
@@ -46,6 +38,5 @@ int get_error(simple_shell_d *simpdata, int eval)
     }
 
     simpdata->status = eval;
-    return (eval);
+    return eval;
 }
-
