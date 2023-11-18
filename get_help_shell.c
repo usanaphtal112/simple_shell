5b0e2496c75ed26fe1274e27a2b1b31d8f26d38d
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-11-17 22:49:26 -0800

    Added changes

--- get_help_shell.c
+++ get_help_shell.c
@@ -12,47 +12,43 @@
  *
  * @return: 1 to indicate successful execution.
  */
+#include "shell.h"
+
+/**
+ * get_help - Display help information for shell commands.
+ * @simpdata: Pointer to the simple_shell_d structure.
+ *
+ * This function displays help information for various shell commands based on
+ * the provided command-line argument. If no argument is provided, general help
+ * information for the shell is displayed. Otherwise, it checks for specific
+ * command names and displays their respective help information. If the command
+ * is not recognized, an error message is displayed
+ *
+ * @return: 1 to indicate successful execution.
+ */
 int get_help(simple_shell_d *simpdata)
 {
-    const char *command = simpdata->args[1];
 
-    if (command == NULL)
-    {
+    if (simpdata->args[1] == 0)
         aux_help_general();
-    }
-    else if (strcmp(command, "setenv") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "setenv") == 0)
         aux_help_setenv();
-    }
-    else if (strcmp(command, "env") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "env") == 0)
         aux_help_env();
-    }
-    else if (strcmp(command, "unsetenv") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "unsetenv") == 0)
         aux_help_unsetenv();
-    }
-    else if (strcmp(command, "help") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "help") == 0)
         aux_help();
-    }
-    else if (strcmp(command, "exit") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "exit") == 0)
         aux_help_exit();
-    }
-    else if (strcmp(command, "cd") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "cd") == 0)
         aux_help_cd();
-    }
-    else if (strcmp(command, "alias") == 0)
-    {
+    else if (_strcmp(simpdata->args[1], "alias") == 0)
         aux_help_alias();
-    }
     else
-    {
-        write(STDERR_FILENO, simpdata->args[0], _strlen(simpdata->args[0]));
-    }
+        write(STDERR_FILENO, simpdata->args[0],
+              _strlen(simpdata->args[0]));
 
     simpdata->status = 0;
-    return 1;
+    return (1);
 }
